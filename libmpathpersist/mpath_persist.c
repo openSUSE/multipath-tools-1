53b63c77666d488ed713163f774b3900be4c0432
Martin Wilck <mwilck@suse.com>
2020-09-25 21:37:16 +0200

    libmpathpersist: initialize mpp->hwe in get_mpvec()
    In __mpath_persistent_reserve_out, we call select_all_tg_pt(),
    which requires mpp->hwe to be set. Initialize it in get_mpvec().
    
    Fixes: 5b54e77 ("mpathpersist: add all_tg_pt option")
    Reviewed-by: Benjamin Marzinski <bmarzins@redhat.com>

--- libmpathpersist/mpath_persist.c
+++ libmpathpersist/mpath_persist.c
@@ -340,11 +340,13 @@ get_mpvec (vector curmp, vector pathvec, char * refwwid)
 		if (refwwid && strncmp (mpp->alias, refwwid, WWID_SIZE - 1))
 			continue;
 
-		if (update_multipath_table(mpp, pathvec, DI_CHECKER) != DMP_OK) {
+		if (update_multipath_table(mpp, pathvec, DI_CHECKER) != DMP_OK ||
+		    update_mpp_paths(mpp, pathvec)) {
 			condlog(1, "error parsing map %s", mpp->wwid);
 			remove_map(mpp, pathvec, curmp, PURGE_VEC);
 			i--;
-		}
+		} else
+			extract_hwe_from_path(mpp);
 	}
 	return MPATH_PR_SUCCESS ;
 }
