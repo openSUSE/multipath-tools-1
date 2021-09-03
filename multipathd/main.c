79e88fd472816a36a90860a5b31103150b2c5dd6
Martin Wilck <mwilck@suse.com>
2021-09-03 15:24:36 +0200

    multipathd: uxlsnr: avoid stalled clients during reconfigure
    Since 47cc1d3 ("multipathd: fix client response for socket
    activation"), we hold back clients while reconfigure is running.
    The idea of 47cc1d3 was to fix the behavior during initial
    start up. When multipathd reconfigures itself during runtime,
    and the reconfiguration takes a long time (a minute or more is
    not unusual in big configurations), clients will time out with
    no response ("timeout receiving packet"). Waiting for reconfigure
    to finish breaks our timeout handling.
    
    Therefore we should only apply the logic of 47cc1d3 during initial
    configuration. In this case, the client that triggered socket
    activation may still encounter a timeout, but there's not much we can
    do about that.
    
    Fixes: 47cc1d3 ("multipathd: fix client response for socket activation")

--- multipathd/main.c
+++ multipathd/main.c
@@ -1699,6 +1699,15 @@ uxlsnrloop (void * ap)
 	set_handler_callback(UNSETMARGINAL+MAP, cli_unset_all_marginal);
 
 	umask(077);
+
+	/*
+	 * Wait for initial reconfiguration to finish, while
+	 * hadling signals
+	 */
+	while (wait_for_state_change_if(DAEMON_CONFIGURE, 50)
+	       == DAEMON_CONFIGURE)
+		handle_signals(false);
+
 	uxsock_listen(&uxsock_trigger, ux_sock, ap);
 
 out_sock:
