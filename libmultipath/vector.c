c1942f4bdc96ff184e15ff68de7963d4e17422c0
Martin Wilck <mwilck@suse.com>
2021-11-29 21:50:38 +0100

    libmultipath: add vector_append_slot() helper
    A very basic helper that could be quite useful.

--- libmultipath/vector.c
+++ libmultipath/vector.c
@@ -197,14 +197,19 @@ vector_set_slot(vector v, void *value)
 	v->slot[i] = value;
 }
 
+int vector_append_slot(vector v, void *value)
+{
+	if (!vector_alloc_slot(v))
+		return -1;
+	vector_set_slot(v, value);
+	return VECTOR_SIZE(v) - 1;
+}
+
 int vector_find_or_add_slot(vector v, void *value)
 {
 	int n = find_slot(v, value);
 
 	if (n >= 0)
 		return n;
-	if (!vector_alloc_slot(v))
-		return -1;
-	vector_set_slot(v, value);
-	return VECTOR_SIZE(v) - 1;
+	return vector_append_slot(v, value);
 }
