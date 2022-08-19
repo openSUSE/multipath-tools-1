45be43b683ef32577f102293229e9998ffb7584c
Hannes Reinecke <hare@suse.de>
2022-08-19 08:29:03 +0200

    multipathd: replace libreadline with getline()
    libreadline changed the license to be incompatible with multipath-tools
    usage, so replace it with a simple getline().
    
    mwilck: Make this the default option via Makefile.inc; it is used if
    READLINE is unset. Compiling with READLINE=libreadline or READLINE=libedit
    remains possible.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>
    Signed-off-by: Martin Wilck <mwilck@suse.com>

--- multipathd/cli.c
+++ multipathd/cli.c
@@ -589,6 +589,7 @@ void cli_exit(void)
 	keys = NULL;
 }
 
+#if defined(USE_LIBREADLINE) || defined(USE_LIBEDIT)
 static int
 key_match_fingerprint (struct key * kw, uint64_t fp)
 {
@@ -694,3 +695,4 @@ key_generator (const char * str, int state)
 	 */
 	return ((char *)NULL);
 }
+#endif
